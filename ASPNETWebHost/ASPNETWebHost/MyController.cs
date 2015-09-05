d21128d3b92b33d3faf0b643a1ba5e0154edcdef
Ravi Kumar Singh <ravikrsingh@outlook.com>
2015-09-05 18:37:21 +0530

    ASP.Net Web Hosting

---
+++ ASPNETWebHost/ASPNETWebHost/MyController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace ASPNETWebHost
+{
+   public class MyController: ApiController
+    {
+       public string Get()
+       {
+           return "Hello from Controller";
+       }
+    }
+}
---
+++ ASPNETWebHost/ASPNETWebHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace ASPNETWebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8999");
+            config.Routes.MapHttpRoute("default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+            var server = new HttpSelfHostServer(config);
+
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Server is Up and Running");
+            Console.ReadLine();
+
+        }
+    }
+}
