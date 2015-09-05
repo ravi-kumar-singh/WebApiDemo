a91db9c32c9b82689ef6c26e57902bfacaccde0c
Ravi Kumar Singh <ravikrsingh@outlook.com>
2015-09-05 18:19:34 +0530

    Web Api Self Hosting

---
+++ WebAPISelfHostingDemo/WebAPISelfHostingDemo/MyHttpMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebAPISelfHostingDemo
+{
+    class MyHttpMessageHandler : HttpMessageHandler
+    {
+        protected override System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Recive an http message");
+            var task = new Task<HttpResponseMessage>(() =>
+            {
+                var msg = new HttpResponseMessage();
+                msg.Content = new StringContent("Hello Self Content");
+                Console.WriteLine("http responce sent");
+                return msg;
+            });
+            task.Start();
+            return task;
+        }
+    }
+}
---
+++ WebAPISelfHostingDemo/WebAPISelfHostingDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http.SelfHost;
+
+namespace WebAPISelfHostingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1 Create HostConfiguration
+            var config = new HttpSelfHostConfiguration("http://localhost:8999");
+
+            var server = new HttpSelfHostServer(config, new MyHttpMessageHandler());
+
+           var task=  server.OpenAsync();
+           task.Wait();
+           Console.WriteLine("Server is Up and Running");
+           Console.ReadLine();
+
+        }
+    }
+}
